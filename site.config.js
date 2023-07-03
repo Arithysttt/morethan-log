2b43b8d27e9db5e9277351276cd42ff1d4612bca
Arithysttt <138498356+Arithysttt@users.noreply.github.com>
2023-07-03 21:46:58 +0800

    site.config.js

--- site.config.js
+++ site.config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Arithyst's Collections",
+    description: "Here is my prores/masters collection",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
